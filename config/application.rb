c72cfd2ee3ddf4f358140b9f21261dece49f55b5
geekelo <efueliht12@gmail.com>
2023-11-12 10:11:06 +0100

    Fixed devise errors

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require_relative "boot"
 
 require "rails"
-require 'rails/all'
-require 'devise'
+require "rails/all"
+require "devise"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
